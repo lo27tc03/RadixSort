3881f09b74beb978d1a8db44b353c89424c00ca3
SPUTBM <Simon@MacBook-Pro-de-Simon.local>
2015-11-02 11:26:23 +0100

    Finish .h

--- BaseNIntegerListOfList.h
+++ BaseNIntegerListOfList.h
@@ -16,13 +16,20 @@ typedef int BOOL;
 BaseNIntegerListOfList createBucketList (int);
 /************************************************************************************
 *
-*caca
+*creates a BaseNIntegerListOfList for storing list of integers in base N 
+*(N being the specified integer, first parameter) 
+*
 *************************************************************************************/
 
 
 BaseNIntegerListOfList buildBucketList(BaseNintegerList, int);
 /*************************************************************************************
 *
+*builds a new BaseNIntegerListOfList according to the specified BaseNIntegerList 
+*and considering the specified digit position (rightmost)
+		Remark : You could use the function strtol 2 for converting a given digit (char*) 
+		represented in a given base into the corresponding digit in decimal
+		base, it could be useful for identifying the right bucket.
 *
 *
 *************************************************************************************/
@@ -31,28 +38,32 @@ BaseNIntegerListOfList buildBucketList(BaseNintegerList, int);
 BaseNintegerList buildIntegerList(BaseNIntegerListOfList);
 /*************************************************************************************
 *
-*
+*builds a new BaseNIntegerList from the specified BaseNIntegerListOfList 
+*(respecting the ascending order on the buckets)
 *
 *************************************************************************************/
 
 BaseNIntegerListOfList addIntegerIntoBucket(BaseNIntegerListOfList , char* , int);
 /*************************************************************************************
 *
-*
+*adds a new integer (char*) at the end of the specified list in bucket N 
+*(N being the third parameter, between 0 and F[or 16])
 *
 *************************************************************************************/
 
 void deleteBucketList(BaseNIntegerListOfList);
 /*************************************************************************************
 *
-*
+*clears and deletes the specified BaseNIntegerListOfList 
+*(free the previously allocated memory)
 *
 *************************************************************************************/
 
 BaseNIntegerListOfList radixsort(BaseNIntegerListOfList);
 /************************************************************************************
 *
-*
+*sorts the specified BaseNIntegerList using the proposed radix sort approach. 
+*See Figures 3-7 for a detailed description of the expected behavior of this function.
 *
 *
 ************************************************************************************/
