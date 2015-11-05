56c8932ed7338035a86ad6a8c1715ad89d12892e
SPUTBM <Simon@MacBook-Pro-de-Simon.local>
2015-11-05 15:40:32 +0100

    Finish .h and add .h
    Finish the first .h
    Create both libraries.c
    
    Code of the first libraries, got some errors, due to pointers and
    arrows

--- BaseNIntegerList.c
+++ BaseNIntegerList.c
@@ -1,5 +1,5 @@
-#ifndef  //typedef de la BaseNintegerList ?? 
-#define
+//#ifndef
+//#define
 
 
 
@@ -25,52 +25,175 @@ typedef struct ListElement
 	int value;
 }ListElement;
 
-BaseNIntegerList createIntegerList (int);
+
+
 /**************************************************************************************
 *
 *Creates a new empty BaseNIntegerList for storing integers in the specified base 
 *Parameters : integer of the base
 *
 *************************************************************************************/
+BaseNIntegerList createIntegerList (int v)
+{
 
-BOOL isEmpty (BaseNIntegerList, );
+	BaseNIntegerList l ;
+	l.head = NULL;
+	l.tail = NULL;
+	l.base = v;
+	l.size = 0;
+
+	return l;
+}
+
+
+
+
+
+
 /*************************************************************************************
 *Test if the list is empty
 *Return 0 if empty, 1 else
 *Parameters : BaseNintegerList to test
 **************************************************************************************/
+BOOL isEmpty (BaseNIntegerList l)
+{
+	return (l.size==0);
+}
 
-BaseNIntegerList insertHead (BaseNIntegerList, char*);
+
+
+
+
 /*************************************************************************************
 *
 *Adds the specified integer (char*, represented in the considered base) at the beginning of the specified list
 *Parameters : BaseNintegerList , integer to add
 *
 **************************************************************************************/
+BaseNIntegerList insertHead (BaseNIntegerList l, char* s)
+{
+	ListElement* newel = (ListElement*)malloc(sizeof(ListElement));
 
-BaseNIntegerList insertTail (BaseNIntegerList, char*);
+	newel.value = s;				// je suis pas sur pour ça     ++    vérification de la base ou pas ? --> autre fonction
+	
+	if (isEmpty(l)==0)
+	{
+		newel.next = NULL;
+		l.tail = newel;
+	}else{
+		newel.next = l.head;
+		(l.head)->previous = newel;
+		l.head = newel;
+	}
+	l.size = l.size + 1;
+	newel.previous = NULL;
+
+	return l;
+}
+
+
+
 /*************************************************************************************
 *
 *Adds the specified integer (char*) at the end of the specified list
 *Parameters : BaseNintegerList , integer to add
 *
 **************************************************************************************/
+BaseNIntegerList insertTail (BaseNIntegerList l, char* s)
+{
+	ListElement newel = (ListElement*)malloc(sizeof(ListElement));
 
-BaseNIntegerList removeHead(BaseNIntegerList);
+	newel.value =  s; 			//IDEM que insertHead
+
+	if (isEmpty(l)==0)
+	{
+		newel.previous = NULL;
+		l.head = newel;
+	}else{
+		newel.previous = l.tail;
+		(l.tail)->next = newel;
+		l.tail = newel
+	}
+	l.size = l.size + 1;
+	newel.next = NULL;
+
+	return l;
+}
+
+
+
+
 /**************************************************************************************
 *
 *Removes the first element of the specified list
 *Parameters : BaseNIntegerList
 *
 *************************************************************************************/
+BaseNIntegerList removeHead(BaseNIntegerList l)
+{
+	if (l.size==0)
+	{
+		return l;
+	}else{
+		if (l.size==1)
+		{
+			free(l.head);
+			l.head = NULL;
+			l.tail = NULL;
+			l.size = 0;
+		}else{
+			BaseNIntegerList* p;			// pas sur niveau allocation si juste pointeur ou structure donc impact aussi le free
+			p.head = l.head;
+			l.head = (l.head)->next;
+			(l.head)->previous = NULL;
+			l.size = l.size - 1;
+			free(p);
+			p = NULL;
+		}
+	}
 
-BaseNIntegerList removeTail(BaseNIntegerList);
+	return l;
+
+}
+
+
+
+
+
+
 /**************************************************************************************
 *
 *Removes the last element of the specified list
 *Parameters : BaseNIntegerList
 *
 *************************************************************************************/
+BaseNIntegerList removeTail(BaseNIntegerList l)
+{
+	if (l.size==0)
+	{
+		return l;
+	}else{
+		if (l.size==1)
+		{
+			free(l.head);
+			l.head = NULL;
+			l.tail = NULL;
+			l.size = 0;
+		}else{
+			BaseNIntegerList* p;			// pas sur niveau allocation si juste pointeur ou structure donc impact aussi le free
+			p.tail = l.tail;
+			l.tail = (l.tail)->previous;
+			(l.tail)->next = NULL;
+			l.size = l.size - 1;
+			free(p);
+			p = NULL;
+		}
+	}
+
+	return l;
+}
+
+
 
 
 void deleteIntegerList(BaseNIntegerList l);
@@ -112,4 +235,4 @@ char* convertBinaryToBase(char* s, int n);
 *
 *************************************************************************************/
 
-#endif 
+//#endif 
